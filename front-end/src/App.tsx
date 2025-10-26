0640bfddb5152a8c6522baefd2387a0a09f66a78
Alex <alex@icouch.me>
2025-10-26 13:31:00 +0000

    tailwind

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [data, setdata] = useState<any[]>([]);
+
+  const doSomething = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/rifugios");
+      const data = await response.json();
+      setdata(data);
+      console.log(data);
+      // const data = await response.json();
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  };
 
   return (
     <>
       <div>Hello World</div>
+      <button onClick={doSomething}>Press me </button>
+      <div className="bg-blue-500">hhshfbsdhf</div>
+      <ul>
+        {data.map((rifugio) => (
+          <li key={rifugio._id}>
+            {rifugio.name}
+            <img src={rifugio.image} alt="" className="w-24 h-24" />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
